d26e6e2e3105f7ea2e1f2861ab518d54d056e067
Moon-Moon-Demadog <lmw2385@email.vccs.edu>
2025-07-12 16:00:58 +0000

    tweaking name string
    name string is weird, IE enter name Green apple and hit enter it will continue adding spaces untill you type agian.

--- .ccls-cache/@home@runner@workspace/main.cpp
+++ .ccls-cache/@home@runner@workspace/main.cpp
@@ -9,10 +9,10 @@ int main()
   int population, days;
   double increase;
   string name;
+  char stop = d;
 
   cout<< "What is the name of your organism :"<< endl;
-  cout<< "\n";
-  getline(cin,name)>> name;
+  getline(cin,name, stop)>> name;
   cout<< "\n";
 
   cout<< "Enter starting population :"<< endl;
@@ -24,5 +24,42 @@ int main()
       cout<< "Invalid input."<< endl;
       cout<< "Enter a number greater than zero :"<< endl;
       cin.clear();
+      cout<< "\n";
+      cin>> population;
     }
+
+  cout<< "Enter daily average percent of population increase :"<< endl;
+  cout<< "\n";
+  cin>> increase;
+  cout<< "\n";
+  while ( increase < 1)
+    {
+      cout<< "Invalid input."<< endl;
+      cout<< "Enter a percentage greater than zero :"<< endl;
+      cin.clear();
+      cout<< "\n";
+      cin>> increase;
+    }
+
+  double increase_Mulitply = increase / 100.00;
+
+  cout<< "How many days are we observing :"<< endl;
+  cout<< "\n";
+  cin>> days;
+  cout<< "\n";
+  while (days < 1)
+    {
+      cout<< "Invalid input."<< endl;
+      cout<< "Enter a number great than zero :"<< endl;
+      cin.clear();
+      cout<< "\n";
+      cin>> days;
+    }
+  double increase_Population = population * increase_Mulitply;
+  double total_Growth = increase_Population * days;
+
+  cout<< "Your organism "<< name << " will reach a population of "<< total_Growth;
+  cout<< " in " << days << " days."<< endl;
+
+  return 0;
 }
