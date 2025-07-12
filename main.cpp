22c29c6cd08286ea19d01a44aad3aee0c7d21657
Moon-Moon-Demadog <lmw2385@email.vccs.edu>
2025-07-12 18:01:11 +0000

    name count
    unsure of how to tackle this and get alphabetical, so I started with line counting first to get number of kids.

--- main.cpp
+++ main.cpp
@@ -1,8 +1,29 @@
 #include <iostream>
 #include <string>
+#include <fstream>
 using namespace std;
 
 int main()
 {
-  
-}
+  ifstream inputFile;
+  string filename;
+  string line;
+  int namecount;
+
+  cout<< "Name of file : " << endl;
+  cin>> filename;
+
+  inputFile.open(filename);
+  if (inputFile.is_open())
+  {
+    while (getline(inputFile,line))
+      {
+        namecount++;
+      }
+    inputFile.close();
+    cout<< "Number of kids : " << namecount << endl;
+  } else {
+    cout<< "ERROR: Could not open file."<< endl;
+  }
+  return 0;
+  }
