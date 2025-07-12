a63d0f528bb0a645e762c13b02ac1bd7a51389ff
Moon-Moon-Demadog <lmw2385@email.vccs.edu>
2025-07-12 18:58:44 +0000

    initial
    adding headers

--- main.cpp
+++ main.cpp
@@ -1,55 +1,12 @@
+// This program will display the year and a bargraph of the population
 #include <iostream>
 #include <string>
 #include <fstream>
 using namespace std;
 
-int main()
+int main ()
 {
-  ifstream inputFile;
-  string filename;
-  string line;
-  string High = "";
-  string Low = "";
-  int namecount = 0;
 
-  cout<< "Name of file : " << endl;
-  cin>> filename;
 
-  inputFile.open(filename);
   
-  if (inputFile.is_open())
-  {
-    while (getline(inputFile,line))
-      {
-        namecount++;
-        if (namecount == 1)
-        {
-          High = line;
-          Low = line;
-        }
-        else
-        {
-          if (line > High)
-          {
-            High = line;
-          }
-          if (line < Low)
-          {
-            Low = line ;
-          }
-        }
-      }
-  }
-  inputFile.close();
-  if (namecount > 0)
-  {
-    cout << "Number of kids :"<< namecount << endl;
-    cout<< High << " is last in line." << endl;
-    cout<< Low << " is first"<< endl;
-  }
-  else
-  {
-    cout<< "ERROR: could not open file."<< endl;
-  }
-  return 0;
 }
