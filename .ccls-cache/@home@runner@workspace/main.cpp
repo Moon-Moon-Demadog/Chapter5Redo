c139481b8089efcb2184bec728324022463d30f5
Moon-Moon-Demadog <lmw2385@email.vccs.edu>
2025-07-12 16:48:14 +0000

    makes a tree
    pattern makes a weird tree, something is wrong with the last statement..

--- .ccls-cache/@home@runner@workspace/main.cpp
+++ .ccls-cache/@home@runner@workspace/main.cpp
@@ -1,10 +1,26 @@
-//This program will display two patterns
-#include <iostream>
-#include <string>
-#include <iomanip>
+# include <iostream>
+# include <string>
 using namespace std;
 
 int main()
 {
-  
+  string pattern_char = "+";
+  string current_pattern = "";
+  int i = 0;
+
+  cout<< "Enjoy this pattern." << endl;
+  while (i<10)
+    {
+      current_pattern += pattern_char;
+      cout<< current_pattern << endl;
+      i++;
+    }
+while (i>0)
+  {
+    cout<< current_pattern<< endl;
+    current_pattern = "";
+    current_pattern += pattern_char;
+    i--;
+  }
+    
 }
