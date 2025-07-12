7d3edef08a0add85c701f9356685adae667901a0
Moon-Moon-Demadog <lmw2385@email.vccs.edu>
2025-07-12 17:30:18 +0000

    testing
    program works!!

--- .ccls-cache/@home@runner@workspace/main.cpp
+++ .ccls-cache/@home@runner@workspace/main.cpp
@@ -1,24 +1,45 @@
-# include <iostream>
-# include <string>
+#include <iostream>
+#include <random>
 using namespace std;
 
 int main()
 {
-  string pattern_char = "+";
-  string current_pattern = "";
-  int i = 0;
+  const int MIN = 0;
+  const int MAX = 100;
+  int guess;
+  int counter = 0;
+  random_device engine;
+  uniform_int_distribution<int>RandomValue(MIN,MAX);
+  int random = RandomValue(engine);
 
-  cout<< "Enjoy this pattern." << endl;
-  while (i<10)
-    {
-      current_pattern += pattern_char;
-      cout<< current_pattern << endl;
-      i++;
-    }
-for(int k = 0; k<10;++k )
+  cout<< "Lets play a game!" << endl;
+  cout<< endl;
+  cout<< " I'm thinking of a number between " << MIN << " & " << MAX ;
+  cout<< " , Enter a number  :"<< endl;
+  cout<< "x \n";
+  cin>> guess;
+
+while (guess < MIN || guess > MAX)
   {
-    cout<< current_pattern<< endl;
-    current_pattern.pop_back();
+    cout<< "Invalid, pleae input a number between " << MIN << " & " << MAX << endl;
+    cin.clear();
+    cout<< "Retry :" << endl;
+    cin>> guess;
   }
-   return 0; 
+    while (guess != random)
+      {
+       ++counter;
+        if (guess < random){
+          cout<< "Too low, Try again :" << endl;  
+        } else {
+          cout<< "Too high, Try again :" << endl;
+        }
+        cin>> guess;
+      }
+  if ( guess == random)
+    {
+      cout<< "Congrats! you got the right answer "<< guess<< " it only took you : "<< counter;
+      cout<< " guesses!"<< endl;
+    }
+  return 0;
 }
